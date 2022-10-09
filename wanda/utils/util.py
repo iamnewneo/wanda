cf999aba95b88ae6f44f998f1fb93d69c4b1acfe
iamnewneo <kunalgusain1995@gmail.com>
2022-10-09 03:01:25 -0400

    Fix

--- wanda/utils/util.py
+++ wanda/utils/util.py
@@ -6,10 +6,10 @@ def get_tranforms():
         [
             transforms.Resize(224),
             transforms.CenterCrop(224),
+            transforms.ToTensor(),
             transforms.Normalize(
                 mean=[0.2112, 0.3528, 0.7894], std=[0.0742, 0.1805, 0.1066]
             ),
             # transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
-            transforms.ToTensor(),
         ]
     )
