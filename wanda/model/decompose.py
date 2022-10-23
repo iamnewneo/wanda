05379bbaf5fb9e12fd4d30411001adcdd098f6a0
iamnewneo <kunalgusain1995@gmail.com>
2022-10-23 00:13:01 -0400

    Fix

--- wanda/model/decompose.py
+++ wanda/model/decompose.py
@@ -1,7 +1,7 @@
 from umap import UMAP
 from sklearn.manifold import Isomap
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import KernelPCA
 from wanda import config
 
 
@@ -19,7 +19,7 @@ class DecomposeData(BaseEstimator, TransformerMixin):
         #     n_jobs=config.N_JOBS,
         # )
         # self.clf = TruncatedSVD(n_components=100, n_iter=20, random_state=42)
-        self.clf = Isomap(n_components=100, n_neighbors=10, n_jobs=config.N_JOBS)
+        self.clf = KernelPCA(n_components=100, random_state=42, n_jobs=config.N_JOBS)
 
     def fit(self, X, y=None):
         self.clf.fit(X)
