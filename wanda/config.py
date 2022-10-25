07fcd8938ddd15d062ff55e8bea29c89713e4811
iamnewneo <kunalgusain1995@gmail.com>
2022-10-25 12:20:03 -0400

    Fix

--- wanda/config.py
+++ wanda/config.py
@@ -16,7 +16,7 @@ configs = {
         "DEVICE": "cuda",
         "BATCH_SIZE": 256,
         "TEST_BATCH_SIZE": 4096,
-        "MAX_EPOCHS": 50,
+        "MAX_EPOCHS": 200,
         "LR": 0.00005,
         "N_WORKER": 8,
         "N_OPT_TRIALS": 100,
