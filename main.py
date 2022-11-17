0e89bcd6d54cea2d6ed0386e09aa74f43da641b5
iamnewneo <kunalgusain1995@gmail.com>
2022-11-17 16:35:48 -0500

    Fix

--- main.py
+++ main.py
@@ -12,11 +12,23 @@ def is_anomaly(spectrogram):
     return is_anomalous
 
 
-def main():
-    img_path = "./data/Test_Data_Set_With_Interference/WiFi2_20/BLU5_0_3/intW2B5_20_0_3_17501_OFF.png"
-    torch_image = load_image(img_path)
+def test_anomly():
+    img_path_anomaly = "./data/Test_Data_Set_With_Interference/WiFi2_20/BLU5_0_2/intW2B5_20_0_2_16311_ON.png"
+    torch_image = load_image(img_path_anomaly)
     is_anomalous = is_anomaly(torch_image)
-    print(f"Is Anomaly? {is_anomalous}")
+    print(f"Actual Anomaly True, Predicted: {is_anomalous}")
+
+
+def test_non_anomaly():
+    img_path_no_anomaly = "./data/Test_Data_Set_With_Interference/WiFi2_20/BLU5_0_3/intW2B5_20_0_3_17501_OFF.png"
+    torch_image = load_image(img_path_no_anomaly)
+    is_anomalous = is_anomaly(torch_image)
+    print(f"Actual Anomaly False, Predicted: {is_anomalous}")
+
+
+def main():
+    test_anomly()
+    test_non_anomaly()
 
 
 if __name__ == "__main__":
